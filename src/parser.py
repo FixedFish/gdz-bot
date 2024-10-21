ffdae9bc1712d7276f9717dcbd5246790fa4c546
FixedFish <fish.raw@rambler.ru>
2024-10-21 20:48:00 +0500

    finally works

--- src/parser.py
+++ src/parser.py
@@ -16,19 +16,21 @@ class Parser:
         page = self._get_html(self.base_url)
         soup = self._get_soup(page)
 
-        csselector = soup.select_one(f".sidebar__main > div:nth-child(1) > ul:nth-child({grade})")
-        return [link.get("href") for link in csselector.find_all("a") if link.get("href") is not None]
+        sctr = soup.select_one(f".sidebar__main > div:nth-child(1) > ul:nth-child({grade})")
+        return [link.get("href") for link in sctr.find_all("a") if link.get("href") is not None]
 
     def get_available_books(self, url):
         new_url = self.base_url + url
         page = self._get_html(new_url)
         soup = self._get_soup(page)
 
-        csselector = soup.select("li.book__item")
-        # todo somehow get available books by grade and item
+        sctr = soup.select("ul.book__list > li.book__item > a.book__link")
+        return [sr.get("href") for sr in sctr]
 
 
 if __name__ == "__main__":
     fish = Parser()
     available_items = fish.get_items_by_grade(9)
-    print(available_items[3])
+    available_books = fish.get_available_books(available_items[1])
+    print(available_items[0])
+    print(available_books)
