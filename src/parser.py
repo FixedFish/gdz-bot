b874985d8d78a3d35a8dfd3c3e59b8106f141b81
FixedFish <fish.raw@rambler.ru>
2024-10-21 21:18:52 +0500

    Refactor

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from typing import List
 
 
 class Parser:
@@ -12,14 +13,14 @@ class Parser:
     def _get_soup(self, html):
         return BeautifulSoup(html, "html.parser")
 
-    def get_items_by_grade(self, grade):
+    def get_subjects_by_grade(self, grade: int) -> List[str]:
         page = self._get_html(self.base_url)
         soup = self._get_soup(page)
 
         sctr = soup.select_one(f".sidebar__main > div:nth-child(1) > ul:nth-child({grade})")
         return [link.get("href") for link in sctr.find_all("a") if link.get("href") is not None]
 
-    def get_available_books(self, url):
+    def get_textbook_by_subject(self, url: str) -> List[str]:
         new_url = self.base_url + url
         page = self._get_html(new_url)
         soup = self._get_soup(page)
@@ -27,10 +28,21 @@ class Parser:
         sctr = soup.select("ul.book__list > li.book__item > a.book__link")
         return [sr.get("href") for sr in sctr]
 
+    def get_solved_task_img(self, url: str) -> List[str]:
+        new_url = self.base_url + url
+        page = self._get_html(new_url)
+        soup = self._get_soup(page)
+
+        sctr = soup.find_all("img")
+        img_path = [sr.get("src") for sr in sctr if sr.get("src") and "/images/task" in sr.get("src")]
+        return ["https:" + url for url in img_path]
+
 
 if __name__ == "__main__":
     fish = Parser()
-    available_items = fish.get_items_by_grade(9)
-    available_books = fish.get_available_books(available_items[1])
+    available_items = fish.get_subjects_by_grade(9)
+    available_books = fish.get_textbook_by_subject(available_items[2])
+    img_link = fish.get_solved_task_img(available_books[1] + "15-nom")
     print(available_items[0])
     print(available_books)
+    print(img_link)
