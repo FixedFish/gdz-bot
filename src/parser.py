1ced927cad9cffcea0c70e4b7428c3b7b7f1729d
FixedFish <fish.raw@rambler.ru>
2024-10-21 20:31:31 +0500

    hate css selectors

--- src/parser.py
+++ src/parser.py
@@ -15,11 +15,20 @@ class Parser:
     def get_items_by_grade(self, grade):
         page = self._get_html(self.base_url)
         soup = self._get_soup(page)
-        sr = soup.select_one(f".sidebar__main > div:nth-child(1) > ul:nth-child({grade})")
-        return [link.get("href") for link in sr.find_all("a") if link.get("href") is not None]
+
+        csselector = soup.select_one(f".sidebar__main > div:nth-child(1) > ul:nth-child({grade})")
+        return [link.get("href") for link in csselector.find_all("a") if link.get("href") is not None]
+
+    def get_available_books(self, url):
+        new_url = self.base_url + url
+        page = self._get_html(new_url)
+        soup = self._get_soup(page)
+
+        csselector = soup.select("li.book__item")
+        # todo somehow get available books by grade and item
 
 
 if __name__ == "__main__":
     fish = Parser()
     available_items = fish.get_items_by_grade(9)
-    print(available_items[0])
+    print(available_items[3])
